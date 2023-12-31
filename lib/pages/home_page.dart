391139c312e6ce7ad1dcd0c74c305c4acad41942
AbdelKarim Medouse <abdelkarim.medouse@gmail.com>
2023-12-31 15:48:15 +0100

    Added search bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:ionicons/ionicons.dart';
-import 'package:travel_app/widgets/custom_icon_button.dart';
+// import 'package:travel_app/widgets/custom_icon_button.dart';
 import 'package:travel_app/widgets/location_card.dart';
 import 'package:travel_app/widgets/nearby_places.dart';
 import 'package:travel_app/widgets/recommended_places.dart';
 import 'package:travel_app/widgets/tourist_places.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+   bool _isSearchVisible = false;
+   final TextEditingController _searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -26,53 +34,99 @@ class HomePage extends StatelessWidget {
             ),
           ],
         ),
-        actions: const [
-          CustomIconButton(
-            icon: Icon(Ionicons.search_outline),
+        actions: [
+          Visibility(
+            visible: !_isSearchVisible,
+            child: IconButton(
+              icon: const Icon(Ionicons.search_outline),
+              onPressed: () {
+                setState(() {
+                  _isSearchVisible = true;
+                });
+              },
+            ),
+          ),
+          Visibility(
+            visible: _isSearchVisible,
+            child: Padding(
+              padding: const EdgeInsets.only(right: 12.0),
+              child: IconButton(
+                icon: const Icon(Icons.close),
+                onPressed: () {
+                  setState(() {
+                    _isSearchVisible = false;
+                    _searchController.clear();
+                  });
+                },
+              ),
+            ),
           ),
           Padding(
-            padding: EdgeInsets.only(left: 8.0, right: 12),
-            child: CustomIconButton(
-              icon: Icon(Ionicons.notifications_outline),
+            padding: const EdgeInsets.only(left: 8.0, right: 12),
+            child: IconButton(
+              icon: const Icon(Ionicons.notifications_outline),
+              onPressed: () {
+                // Add your notification button logic here
+              },
             ),
           ),
         ],
       ),
-      body: ListView(
-        physics: const BouncingScrollPhysics(),
-        padding: const EdgeInsets.all(14),
+      body: Column(
         children: [
-          // LOCATION CARD
-          const LocationCard(),
-          const SizedBox(height: 15),
-          const TouristPlaces(),
-          // CATEGORIES
-          const SizedBox(height: 10),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                "Recommendation",
-                style: Theme.of(context).textTheme.titleLarge,
+          if (_isSearchVisible)
+            Padding(
+              padding: const EdgeInsets.all(14),
+              child: TextField(
+                controller: _searchController,
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Ionicons.search_outline),
+                  hintText: 'Search...',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
               ),
-              TextButton(onPressed: () {}, child: const Text("View All"))
-            ],
+            ),
+          Expanded(
+            child: ListView(
+              physics: const BouncingScrollPhysics(),
+              padding: const EdgeInsets.all(14),
+              children: [
+                // LOCATION CARD
+                const LocationCard(),
+                const SizedBox(height: 15),
+                const TouristPlaces(),
+                // CATEGORIES
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Recommendation",
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    TextButton(onPressed: () {}, child: const Text("View All"))
+                  ],
+                ),
+                const SizedBox(height: 10),
+                const RecommendedPlaces(),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Nearby From You",
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    TextButton(onPressed: () {}, child: const Text("View All"))
+                  ],
+                ),
+                const SizedBox(height: 10),
+                const NearbyPlaces(),
+              ],
+            ),
           ),
-          const SizedBox(height: 10),
-          const RecommendedPlaces(),
-          const SizedBox(height: 10),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                "Nearby From You",
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-              TextButton(onPressed: () {}, child: const Text("View All"))
-            ],
-          ),
-          const SizedBox(height: 10),
-          const NearbyPlaces(),
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
@@ -88,10 +142,7 @@ class HomePage extends StatelessWidget {
             icon: Icon(Ionicons.bookmark_outline),
             label: "Bookmark",
           ),
-          // BottomNavigationBarItem(
-          //   icon: Icon(Ionicons.ticket_outline),
-          //   label: "Ticket",
-          // ),
+
           BottomNavigationBarItem(
             icon: Icon(Ionicons.person_outline),
             label: "Profile",
